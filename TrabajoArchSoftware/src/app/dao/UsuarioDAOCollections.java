5a60c0da86fe90cb56d5eff1ef5bf2abb6b9b953
eloyUA <eurriens@gmail.com>
2026-03-11 10:43:16 +0100

    fix: Dejo todo unido

--- TrabajoArchSoftware/src/app/dao/UsuarioDAOCollections.java
+++ TrabajoArchSoftware/src/app/dao/UsuarioDAOCollections.java
@@ -4,12 +4,11 @@ import app.modelos.Usuario;
 import java.util.ArrayList;
 import java.util.List;
 
-public class UsuarioDAOCollections implements UsuarioDAO {
+public class UsuarioDAOCollections implements UsuarioDAO{
     
     private List<Usuario> usuarios = new ArrayList<>();
 
     public UsuarioDAOCollections() {
-        
     }
 
     @Override
@@ -23,20 +22,29 @@ public class UsuarioDAOCollections implements UsuarioDAO {
             }
         }
         
-        if(estaEnLista)
+        if(estaEnLista == false)
             usuarios.add(usuario);
     }
 
     @Override
     public List<Usuario> getUsuarios() {
+        return new ArrayList<>(usuarios);
     }
 
     @Override
     public Usuario getUsuario(int idSeleccionado) {
+        for(int i = 0; i  < usuarios.size(); i++){
+            if(usuarios.get(i).getId() == idSeleccionado)
+                return usuarios.get(i);
+        }  
+        return null;
     }
 
     @Override
     public void actualizarUsuario(Usuario usuario) {
+        for(int i = 0; i  < usuarios.size(); i++){
+            if(usuarios.get(i).getId() == usuario.getId())
+                usuarios.set(i,usuario);
+        }  
     }
-
 }
