445d77adad6a51fa8fe7c6732adf9c765974cb5c
Q Muñoz Presentacion <emunozpresentacion@gmail.com>
2026-03-11 11:26:17 +0100

    Update AmigosVista.java

--- TrabajoArchSoftware/AmigosVista.java
+++ TrabajoArchSoftware/AmigosVista.java
@@ -3,7 +3,7 @@ package app.vistas;
 
 public interface AmigosVista {
     public void open();
-    public void cambiarNombre(int idUsuario, String nombre);
+    public void cambiarNombre(Usuario idUsuario, String nombre);
     public void agregarAmigo(int idAmigo);
     public void close();
 }
